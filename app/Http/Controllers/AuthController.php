a874228a2412269c8472f2ec553034e67b4c4b5a
WendriTP <wendritrip4321@gmail.com>
2020-08-06 10:46:22 +0700

    add endpoint /me

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller {
+
+    public function me() {
+        return [
+            "nis" => 3103118136,
+            "name" => "Wendri Tri Pambudi",
+            "gender" => "Laki-laki",
+            "phone" => 6282226481208,
+            "class" => "XII RPL 4"
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+ return "XII RPL 4"; 
 });
+
+Route::get('/me', 'AuthController@me'); 
